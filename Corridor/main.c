2380806d4a162f1b0f7a347045a84a38b307e265
Hinode Coder <luke@hinodecode.com>
2021-11-06 13:33:19 +0100

    Initial Commit

---
+++ Corridor/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Corridor
+//
+//  Created by Łukasz Tkacz on 06/11/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
